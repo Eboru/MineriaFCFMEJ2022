8fcf2f58fd011fc82c9eccf0d04785756cc53cc6
Eboru <garzagonzalez_08@hotmail.com>
2022-02-26 19:07:22 -0600

    estadistica descriptiva

--- Practica3/Analysis1.py
+++ Practica3/Analysis1.py
@@ -25,15 +25,62 @@ joinChar = df.join(heroes.set_index("id"), on="idHero")
 df_analisisChar = joinChar.groupby(["name"])[["kills", "deaths", "assists"]].mean().sort_values(by=["kills"], ascending=False)
 df_analisis_tower_damage = joinChar.groupby(["name"])[["towerDamage", "nethWorth"]].mean().sort_values(by=["towerDamage"], ascending=False)
 
-matchCount = joinMatch[["idMatchInfo"]].count().min()
-df_popularity_per_match = joinChar.groupby(["name"])[["idPartida"]].count()
-df_popularity_per_match["idPartida"] = df_popularity_per_match["idPartida"].transform(match_average)
-df_popularity_per_match = df_popularity_per_match.sort_values(by=["idPartida"], ascending=False)
-df_popularity_per_match.rename({"idPartida": "Porcentaje de eleccion"}, axis="columns", inplace=True)
+df_analisisPorcentajeVictoria = joinMatch.groupby(["name"])[["radiant_win"]].mean().sort_values(by=["radiant_win"], ascending=False)
+df_analisisPorcentajeVictoria.rename({"idPartida": "Porcentaje de victoria"}, axis="columns", inplace=True)
 
-df_matchDurationAndChar = joinMatch.groupby(["name", "timeClassification"])[["idPartida"]].count()
+matchCount = joinMatch[["idMatchInfo"]].count().min() #Contamos todas las partidas
+df_popularity_per_match = joinChar.groupby(["name"])[["idPartida"]].count() #Agrupamos por nombre y contamos cuantas veces aparece
+df_popularity_per_match["idPartida"] = df_popularity_per_match["idPartida"].transform(match_average) #Dividimos
+df_popularity_per_match = df_popularity_per_match.sort_values(by=["idPartida"], ascending=False)
+df_popularity_per_match.rename({"idPartida": "Porcentaje de eleccion global"}, axis="columns", inplace=True)
+
+df_matchDurationAndChar = joinMatch
+VERY_LOW = []
+LOW = []
+MEDIUM = []
+HIGH = []
+VERY_HIGH = []
+
+for index, row in joinMatch.iterrows():
+
+    if row["timeClassification"] == "VERY_LOW":
+        VERY_LOW.append(1)
+    else:
+        VERY_LOW.append(0)
+
+    if row["timeClassification"] == "LOW":
+        LOW.append(1)
+    else:
+        LOW.append(0)
+
+    if row["timeClassification"] == "MEDIUM":
+        MEDIUM.append(1)
+    else:
+        MEDIUM.append(0)
+
+    if row["timeClassification"] == "HIGH":
+        HIGH.append(1)
+    else:
+        HIGH.append(0)
+
+    if row["timeClassification"] == "VERY_HIGH":
+        VERY_HIGH.append(1)
+    else:
+        VERY_HIGH.append(0)
+
+df_matchDurationAndChar["VERY_LOW"] = VERY_LOW
+df_matchDurationAndChar["LOW"] = LOW
+df_matchDurationAndChar["MEDIUM"] = MEDIUM
+df_matchDurationAndChar["HIGH"] = HIGH
+df_matchDurationAndChar["VERY_HIGH"] = VERY_HIGH
+
+df_matchDurationAndChar = joinMatch.groupby(["name"])[["VERY_LOW", "LOW", "MEDIUM", "HIGH", "VERY_HIGH"]].sum().sort_values(by=["HIGH"], ascending=False)
 df_matchDurationAndChar.rename({"idPartida": "Cantidad"}, axis="columns", inplace=True)
 
+df_charDesviation = joinChar.groupby(["name"])[["kills", "deaths", "assists"]].std().sort_values(by=["kills"], ascending=False)
+
+print_tabulate(df_analisisPorcentajeVictoria)
+print("\n")
 print_tabulate(df_popularity_per_match)
 print("\n")
 print_tabulate(df_analisisChar)
@@ -41,3 +88,5 @@ print("\n")
 print_tabulate(df_analisis_tower_damage)
 print("\n")
 print_tabulate(df_matchDurationAndChar)
+print("\n")
+print_tabulate(df_charDesviation)
