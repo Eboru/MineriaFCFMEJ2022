9bbf44ce113c12d881242c6a94842ca349f848ef
Eboru <garzagonzalez_08@hotmail.com>
2022-03-16 20:10:26 -0600

    mannwhitneyu

--- Practica5/Analysis2.py
+++ Practica5/Analysis2.py
@@ -88,11 +88,26 @@ def signTest(name : str, sr1: pd.Series):
     else:
         print(name, ": Aceptamos la hipotesis nula -> u <=", mediaMuestral)
 
-def wilcoxowSignTest(name : str, sr1: pd.Series, sr2: pd.Series):
+#Como hay ties no esta bueno usar esto : c
+def wilcoxowRankSumTest(name : str, sr1: pd.Series, sr2: pd.Series):
     if sr1.size == 0 or sr2.size==0:
         return
-    if sr1.size != sr2.size:
-        return
+    rank = pd.concat([sr1,sr2]).rank()
+    r1 = pd.Series(rank.array[0:sr1.size]).sum()
+    r2 = pd.Series(rank.array[sr1.size::]).sum()
+    n1 = sr1.size
+    n2 = sr2.size
+    n1n2 = n1*n2
+    u1 = n1n2 + (sr1.size*(sr1.size+1))/2 - r1
+    u2 = n1n2 + (sr2.size * (sr2.size + 1)) / 2 - r2
+
+    u = min(u1,u2)
+    mv = n1n2/2
+    ov = math.sqrt((n1*n2*(n1+n2+1))/12)
+    z = (u-mv)/ov
+
+
+
 
 
 
@@ -105,7 +120,7 @@ df.rename({"Unnamed: 0": "idPlayerInfo"}, axis="columns", inplace=True)
 match.rename({"Unnamed: 0": "idMatchInfo"}, axis="columns", inplace=True)
 
 joinMatch = df.join(match.set_index("match_id"), on="idPartida").join(heroes.set_index("id"), on="idHero")
-joinChar = df.join(heroes.set_index("id"), on="idHero")
+joinChar = df.join(heroes.set_index("id"), on="idHero").join(match.set_index("match_id"), on="idPartida")
 
 
 #Normality test for deaths for each hero
@@ -115,17 +130,17 @@ joinChar = df.join(heroes.set_index("id"), on="idHero")
 #    df_query = joinChar.query(queryStr)
 #    testNormality(row_h["name"], df_query["deaths"])
 
-#Plot xpPerMin distribution
+##Plot xpPerMin distribution
 #fig = plt.figure(figsize=(16, 9))
 #ax1 = fig.add_subplot()
 #ax1.hist(joinChar["xpPerMin"], label="xpPerMin", alpha=1, bins=25)
 #ax1.set_title("xpPerMin")
 #plt.savefig("img/xpPerMin.png")
 #plt.close()
-# Normality test for xp per min (Anderson-Darling)
+##Normality test for xp per min (Anderson-Darling)
 #testNormality("xpPerMin", joinChar["xpPerMin"])
 
-#Plot kills distribution
+##Plot kills distribution
 #fig = plt.figure(figsize=(16, 9))
 #ax1 = fig.add_subplot()
 #ax1.hist(joinChar["kills"], label="kills", alpha=1, bins=25)
@@ -135,19 +150,40 @@ joinChar = df.join(heroes.set_index("id"), on="idHero")
 ## Normality test for kills (Anderson-Darling)
 #testNormality("kills", joinChar["kills"])
 
-#Plot kills-xpPerMin histogram
-fig = plt.figure(figsize=(16, 9))
-ax1 = fig.add_subplot()
-ax1.hist2d(joinChar["kills"], joinChar["xpPerMin"], label="kills", alpha=1, bins=50)
-ax1.set_title("Kills")
-plt.gca().xaxis.set_major_locator(mticker.MultipleLocator(1))
-plt.savefig("img/kills-xpPerMin.png")
-plt.close()
-#Test against Spearman's rank correlation coefficient
-spearmanRankCorrelation("Spearman's Correlation between xpPerMin and Kills", joinChar["kills"], joinChar["xpPerMin"])
-#Test agains Pearson's correlation coefficient
-pearsonCorrelation("Pearson's Correlation between xpPerMin and Kills", joinChar["kills"], joinChar["xpPerMin"])
-#spearman -> posible relación monotona positiva
-#pearson  -> posible relacion lineal positiva
-signTest("Kills", joinChar["kills"])
-signTest("XpPerMin", joinChar["xpPerMin"])
+##Plot kills-xpPerMin histogram
+#fig = plt.figure(figsize=(16, 9))
+#ax1 = fig.add_subplot()
+#ax1.hist2d(joinChar["kills"], joinChar["xpPerMin"], label="kills", alpha=1, bins=50)
+#ax1.set_title("Kills")
+#plt.gca().xaxis.set_major_locator(mticker.MultipleLocator(1))
+#plt.savefig("img/kills-xpPerMin.png")
+#plt.close()
+##Test against Spearman's rank correlation coefficient
+#spearmanRankCorrelation("Spearman's Correlation between xpPerMin and Kills", joinChar["kills"], joinChar["xpPerMin"])
+##Test agains Pearson's correlation coefficient
+#pearsonCorrelation("Pearson's Correlation between xpPerMin and Kills", joinChar["kills"], joinChar["xpPerMin"])
+##spearman -> posible relación monotona positiva
+##pearson  -> posible relacion lineal positiva
+#signTest("Kills", joinChar["kills"])
+#signTest("XpPerMin", joinChar["xpPerMin"])
+
+
+
+##Como hay ties en los datos no podemos usar suma de rango de wilcoxon, entonces tenemos q usar mann-whitney
+##Significancia de 0.05
+##H0: u1=u2
+##H1: u1<u2
+#df_high_broodmother = joinChar.query("name=='Broodmother' & timeClassification=='HIGH'")
+#df_high_spectre = joinChar.query("name=='Spectre' & timeClassification=='HIGH'")
+
+#testNormality("high Broodmother", df_high_broodmother["duration"])
+#testNormality("high Spectre", df_high_spectre["duration"])
+#print("Media Broodmother", df_high_broodmother["duration"].mean())
+#print("Media Spectre", df_high_spectre["duration"].mean())
+
+#statistics, pvalue = ss.mannwhitneyu(df_high_broodmother["duration"], df_high_spectre["duration"], alternative="two-sided")
+#if pvalue <= 0.05: #Rechazar h0
+#    print("Hay suficiente evidencia que indique que media u2(spectre) es mayor que u1(broodmother)", pvalue)
+#else: #Aceptar h0
+#    print("No hay suficiente evidencia que indique que media u2(spectre) es mayor que u1(broodmother", pvalue)
+
